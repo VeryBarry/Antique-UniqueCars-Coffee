37fcf84133f9ae27d350c0580cafadb6dc2bc139
Your Name <w.allanson2@gmail.com>
2016-10-20 13:21:42 -0400

    changes to js

--- public/js/app.js
+++ public/js/app.js
@@ -4,58 +4,58 @@ console.log(_)
 console.log(Backbone)
 
 
-
+//
 var routerController = function(){
    var currentHash = window.location.hash.slice(1)
     console.log(currentHash.length)
    if(currentHash.length === 0) { return showHomePage() }
 
    var currentHashComponents = currentHash.split('/')
-  //  console.log(currentHashPrefix)
-//
+   console.log(currentHashComponents)
+// //
    var currentHashPrefix = currentHashComponents[0]
    var currentHashSuffix = currentHashComponents[1]
    console.log(currentHashPrefix)
   //  console.log(currentHashSuffix)
-   switch(currentHashPrefix){
+    switch(currentHashPrefix){
       case "auth":
          showAuthPage();
          break;
 
-      // case "user-profile":
-      //    showUserProfile(currentHashSuffix);
-      //    break;
-      //
-      // default:
-        //  document.querySelector('#app-container').innerHTML = "<h1 class='bg-warning'>PAGE NOT FOUND</h1>";
-   }
-
+//       // case "user-profile":
+//       //    showUserProfile(currentHashSuffix);
+//       //    break;
+//       //
+//       // default:
+//         //  document.querySelector('#app-container').innerHTML = "<h1 class='bg-warning'>PAGE NOT FOUND</h1>";
+    }
+//
 }
-//
-// var authenticateUser = function(evt){
-//    evt.preventDefault()
-//    console.log("email", evt.target.email.value)
-//    console.log("passord", evt.target.password.value)
-//    var dataForServer = {
-//       email: evt.target.email.value,
-//       password: evt.target.password.value
-//    }
-//
-//    console.log('to server:', JSON.stringify(dataForServer))
-//
-//    $.post( '/login', JSON.stringify(dataForServer) ).then(function(godKnowsWhat){
-//       console.log('Success !!!!')
-//       window.location.hash = "user-profile/"+dataForServer.email
-//    })
-//
-// }
-//
-// var signInPage = function (){
+// //
+// // var authenticateUser = function(evt){
+// //    evt.preventDefault()
+// //    console.log("email", evt.target.email.value)
+// //    console.log("passord", evt.target.password.value)
+// //    var dataForServer = {
+// //       email: evt.target.email.value,
+// //       password: evt.target.password.value
+// //    }
+// //
+// //    console.log('to server:', JSON.stringify(dataForServer))
+// //
+// //    $.post( '/login', JSON.stringify(dataForServer) ).then(function(godKnowsWhat){
+// //       console.log('Success !!!!')
+// //       window.location.hash = "user-profile/"+dataForServer.email
+// //    })
+// //
+// // }
+// //
+// var authPage = function (){
 //    showAuthPage()
-//
+// // //
 // }
-// var signInBtn = document.querySelector('.sign-in-btn')
-//
+// // var signInBtn = document.querySelector('.sign-in-btn')
+// //
 window.addEventListener('hashchange', routerController)
-// document.addEventListener('click',  signInPage)
+// document.addEventListener('click',  authPage)
 routerController()
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,7 +4,7 @@ var showHomePage = function() {
   bigStr +=   '<div class="jumbotron main-header">'
   bigStr +=     '<h1><span style="font-size:90px">Antique and Unique</span><br/>Cars and Coffee</h1>'
   bigStr +=     '<h3>The place where Jalopies and Java come together!</h3>'
-  bigStr +=      '<p class="sign-in-btn"><a class="btn btn-primary btn-lg" href="#auth" role="button">Sign In</a></p>'
+  // bigStr +=      '<p class="sign-in-btn"><a class="btn btn-primary btn-lg" href="#auth" role="button">Sign In</a></p>'
   bigStr +=   '</div>' //<!-- End jumbotron -->
   bigStr +=   '<div class="container">'
   bigStr +=     '<div class="row">'
@@ -20,11 +20,29 @@ var showHomePage = function() {
   bigStr +=           '<input type="text" class="form-control name-input car-model" placeholder="Car model" aria-describedby="sizing-addon1">'
   bigStr +=           '<input type="text" class="form-control name-input car-year" placeholder="Car year" aria-describedby="sizing-addon1">'
   bigStr +=           '<input type="text" class="form-control name-input pswd-input" placeholder="Password" aria-describedby="sizing-addon1">'
-  bigStr +=           '<p class="enter-btn"><a class="btn btn-primary btn-lg" href="#" role="button">Click Me</a></p>'
+  bigStr +=           '<a href="#auth"<button type="button" class="btn btn-default sign-up-btn">sign up</button></a>'
   bigStr +=         '</div>'
   bigStr +=       '</div>'
   bigStr +=     '</div>' //<!-- End of row -->
   bigStr +=   '</div>' //<!-- End of container -->
+  bigStr +=   '<div class ="container photo-container">'
+  bigStr +=     '<div class ="row">'
+  bigStr +=       '<div class="col-sm-3">'
+  bigStr +=         '<img src="images/Ford-Pinto.jpg"/>'
+  bigStr +=       '</div>'
+  bigStr +=       '<div class="col-sm-3">'
+  bigStr +=         '<img src="images/6G6FWUh.jpg"/>'
+  bigStr +=       '</div>'
+  bigStr +=       '<div class="col-sm-3">'
+  bigStr +=           '<img src="images/vanArtHeavyDotCom-460x279.png"/>'
+  bigStr +=       '</div>'
+  bigStr +=       '<div class="col-sm-3">'
+  bigStr +=         '<img src="images/Very funky custome sport car picture.png"/>'
+  bigStr +=       '</div>'
+  bigStr +=     '</div>'
+  bigStr +=   '</div>'
+
+
 
   document.querySelector('#app-container').innerHTML = bigStr
   var signInBtn = document.querySelector('.sign-in-btn')
