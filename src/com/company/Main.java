fb1615b0713fe29dddba6ae3f1169d02f565b13d
VeryBarry <barry.a.daniels@gmail.com>
2016-10-20 14:57:50 -0400

    we haz jar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -123,15 +123,14 @@ public class Main {
     }
     public static ArrayList<Car> selectCars(Connection conn) throws SQLException {
         ArrayList<Car> cars = new ArrayList<>();
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM cars INNER JOIN users ON cars.user_id = users.id");
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM cars");
         ResultSet results = stmt.executeQuery();
         while (results.next()) {
             int id = results.getInt("cars.id");
             String make = results.getString("cars.make");
             String model = results.getString("cars.model");
             int year = results.getInt("cars.year");
-            Car c = new Car(id, make, model, year);
-            cars.add(c);
+            cars.add(new Car(id, make, model, year));
         }
         return cars;
     }
