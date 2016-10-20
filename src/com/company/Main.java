2c13aad5504159a928d0a415d5ce15065fb62633
VeryBarry <barry.a.daniels@gmail.com>
2016-10-20 13:43:55 -0400

    we haz jar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,7 +33,7 @@ public class Main {
                     }
                     Session session = request.session();
                     session.attribute("email", user.username);
-                    return "";
+                    return "Logged In";
                 }
         );
 
@@ -57,7 +57,7 @@ public class Main {
                     Session session = request.session();
                     session.invalidate();
                     response.redirect("/");
-                    return null;
+                    return "Logged Out";
                 }
         );
 
@@ -71,7 +71,7 @@ public class Main {
                     JsonParser parser = new JsonParser();
                     Car car = parser.parse(body, Car.class);
                     insertCar(conn, car, user.id);
-                    return "";
+                    return "Car Added";
                 }
         );
         Spark.get(
@@ -87,7 +87,7 @@ public class Main {
                     int id = Integer.valueOf((request.queryParams("id")));
                     deleteCar(conn, id);
                     response.redirect("/");
-                    return null;
+                    return "Car Deleted";
                 }
         );
     }
