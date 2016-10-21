0a097e3f9b706bc06f67e65b547406cfee95120d
Your Name <w.allanson2@gmail.com>
2016-10-21 15:32:16 -0400

    finally working with b/e

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,10 @@
 // console.log($)
 // console.log(_)
 // console.log(Backbone)
-var BACKEND_URL = "https://quik-spitter-api.herokuapp.com/api"
-// var BACKEND_URL = "http://localhost:4567"
+// var BACKEND_URL = "https://quik-spitter-api.herokuapp.com/api"
+var BACKEND_URL = ""
 
-//
+
 var routerController = function(){
    var currentHash = window.location.hash.slice(1)
     // console.log("hash length", currentHash.length)
@@ -14,12 +14,12 @@ var routerController = function(){
      var newCarForm = document.querySelector('#new-car-form')
      newCarForm.addEventListener('submit',  saveUserCarInfo)
       // console.log(newCarForm)
-      return
+      return;
    }
 
    var currentHashComponents = currentHash.split('/')
   //  console.log("components:", currentHashComponents)
-// //
+
    var currentHashPrefix = currentHashComponents[0]
    var currentHashSuffix = currentHashComponents[1]
 
@@ -28,17 +28,14 @@ var routerController = function(){
     switch(currentHashPrefix){
 
       case "listing":
-
         $.getJSON(BACKEND_URL + "/car").then(function(serverRes){
 
           showListingPage(serverRes);
           console.log(serverRes)
           document.querySelector('#app-container').innerHTML
       })
-
           // showListingPage(serverRes)
           // console.log("serRes", serverRes)
-        // })
         break;
     }
 }
@@ -54,23 +51,24 @@ var saveUserCarInfo = function(evt){
   var modelFormVal = evt.target.model.value
   var yearFormVal = evt.target.year.value
   // console.log(makeFormVal)
+
   var objForDatabase = {
     "make": makeFormVal,
     "model": modelFormVal,
     "year": yearFormVal
   }
   // console.log(objForDatabase)
+
   var jsonStr = JSON.stringify(objForDatabase)
 
   var postReqSettings =  {
       url: BACKEND_URL + '/add-car',
       data: jsonStr,
-      dataType: 'json',
       headers: {'Content-Type': 'application/json'}
     }
 
-  //$.post('/add-car', jsonStr)
   $.post(postReqSettings)
+  // $.post('/add-car', JSON.stringify(postReqSettings))
     .then(function(serverRes){
       // console.log(serverRes)
       window.location.hash = "listing"
