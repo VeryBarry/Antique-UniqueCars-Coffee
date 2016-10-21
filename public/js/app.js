8201aa7f9f6386669614b16a5b6d357e47fa67e9
Your Name <w.allanson2@gmail.com>
2016-10-20 21:26:03 -0400

    added JSON

--- public/js/app.js
+++ public/js/app.js
@@ -2,60 +2,76 @@ console.log('wired up!')
 console.log($)
 console.log(_)
 console.log(Backbone)
-
+var BACKEND_URL = "https://quik-spitter-api.herokuapp.com/api"
+// var BACKEND_URL = ""
 
 //
 var routerController = function(){
    var currentHash = window.location.hash.slice(1)
-    console.log(currentHash.length)
-   if(currentHash.length === 0) { return showHomePage() }
+    console.log("hash length", currentHash.length)
+   if(currentHash.length === 0) {
+     showHomePage()
+     var newCarForm = document.querySelector('#new-car-form')
+     newCarForm.addEventListener('submit',  saveUserCarInfo)
+      console.log(newCarForm)
+      return
+   }
 
    var currentHashComponents = currentHash.split('/')
-   console.log(currentHashComponents)
+   console.log("components:", currentHashComponents)
 // //
    var currentHashPrefix = currentHashComponents[0]
    var currentHashSuffix = currentHashComponents[1]
-   console.log(currentHashPrefix)
+
+   console.log("prefix:", currentHashPrefix)
   //  console.log(currentHashSuffix)
     switch(currentHashPrefix){
       case "auth":
          showAuthPage();
          break;
+      case "listing":
+        $.getJSON(BACKEND_URL + "/car").then(function(serverRes){
+          console.log(serverRes)
+          document.querySelector('#app-container').innerHTML = '<h2>hello</h2>'
 
-//       // case "user-profile":
-//       //    showUserProfile(currentHashSuffix);
-//       //    break;
-//       //
-//       // default:
-//         //  document.querySelector('#app-container').innerHTML = "<h1 class='bg-warning'>PAGE NOT FOUND</h1>";
+      })
     }
-//
 }
-// //
-// // var authenticateUser = function(evt){
-// //    evt.preventDefault()
-// //    console.log("email", evt.target.email.value)
-// //    console.log("passord", evt.target.password.value)
-// //    var dataForServer = {
-// //       email: evt.target.email.value,
-// //       password: evt.target.password.value
-// //    }
-// //
-// //    console.log('to server:', JSON.stringify(dataForServer))
-// //
-// //    $.post( '/login', JSON.stringify(dataForServer) ).then(function(godKnowsWhat){
-// //       console.log('Success !!!!')
-// //       window.location.hash = "user-profile/"+dataForServer.email
-// //    })
-// //
-// // }
-// //
-// var authPage = function (){
-//    showAuthPage()
-// // //
-// }
-// // var signInBtn = document.querySelector('.sign-in-btn')
-// //
+
+
+var saveUserCarInfo = function(evt){
+  evt.preventDefault()
+  // console.log(evt.target)
+  document.querySelector('.car-make')
+  document.querySelector('.car-model')
+  document.querySelector('.car-year')
+  console.log(evt.target)
+  var makeFormVal = evt.target.make.value
+  var modelFormVal = evt.target.model.value
+  var yearFormVal = evt.target.year.value
+  console.log(makeFormVal)
+  var objForDatabase = {
+    "make": makeFormVal,
+    "model": modelFormVal,
+    "year": yearFormVal
+  }
+  console.log(objForDatabase)
+  var jsonStr = JSON.stringify(objForDatabase)
+
+  var postReqSettings =  {
+      url: BACKEND_URL + '/add-car',
+      data: jsonStr,
+      dataType: 'json',
+      headers: {'Content-Type': 'application/json'}
+    }
+
+  //$.post('/add-car', jsonStr)
+  $.post(postReqSettings)
+    .then(function(serverRes){
+      console.log(serverRes)
+      window.location.hash = "listing"
+    })
+}
+
 window.addEventListener('hashchange', routerController)
-// document.addEventListener('click',  authPage)
 routerController()
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,7 +4,6 @@ var showHomePage = function() {
   bigStr +=   '<div class="jumbotron main-header">'
   bigStr +=     '<h1><span style="font-size:90px">Antique and Unique</span><br/>Cars and Coffee</h1>'
   bigStr +=     '<h3>The place where Jalopies and Java come together!</h3>'
-  // bigStr +=      '<p class="sign-in-btn"><a class="btn btn-primary btn-lg" href="#auth" role="button">Sign In</a></p>'
   bigStr +=   '</div>' //<!-- End jumbotron -->
   bigStr +=   '<div class="container">'
   bigStr +=     '<div class="row">'
@@ -13,15 +12,13 @@ var showHomePage = function() {
   bigStr +=         '</div>'
   bigStr +=       '</div>'
   bigStr +=       '<div class="col-xs-12 col-sm-6 input-container">'
-  bigStr +=         '<div class="input-group input-group-lg input-box">'
+  bigStr +=         '<form class="input-group input-group-lg input-box" id="new-car-form">'
   bigStr +=           '<h3>Join Us Today!</h3>'
-  bigStr +=           '<input type="text" class="form-control screenName-input" placeholder="Screen Name" aria-describedby="sizing-addon1">'
-  bigStr +=           '<input type="text" class="form-control name-input car-make" placeholder="Car make" aria-describedby="sizing-addon1">'
-  bigStr +=           '<input type="text" class="form-control name-input car-model" placeholder="Car model" aria-describedby="sizing-addon1">'
-  bigStr +=           '<input type="text" class="form-control name-input car-year" placeholder="Car year" aria-describedby="sizing-addon1">'
-  bigStr +=           '<input type="text" class="form-control name-input pswd-input" placeholder="Password" aria-describedby="sizing-addon1">'
-  bigStr +=           '<a href="#auth"<button type="button" class="btn btn-default sign-up-btn">sign up</button></a>'
-  bigStr +=         '</div>'
+  bigStr +=           '<input name="make" type="text" class="form-control name-input car-make" placeholder="Car make" aria-describedby="sizing-addon1">'
+  bigStr +=           '<input name="model" type="text" class="form-control name-input car-model" placeholder="Car model" aria-describedby="sizing-addon1">'
+  bigStr +=           '<input name="year" type="text" class="form-control name-input car-year" placeholder="Car year" aria-describedby="sizing-addon1">'
+  bigStr +=           '<button type="submit" class="btn btn-default sign-up-btn">Submit</button>'
+  bigStr +=         '</form>'
   bigStr +=       '</div>'
   bigStr +=     '</div>' //<!-- End of row -->
   bigStr +=   '</div>' //<!-- End of container -->
@@ -45,5 +42,5 @@ var showHomePage = function() {
 
 
   document.querySelector('#app-container').innerHTML = bigStr
-  var signInBtn = document.querySelector('.sign-in-btn')
+  // var signInBtn = document.querySelector('.sign-in-btn')
 }
