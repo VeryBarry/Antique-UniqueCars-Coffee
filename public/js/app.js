5f9d87c75968583f8c095346bf76641d18c57a7c
Your Name <w.allanson2@gmail.com>
2016-10-21 20:13:03 -0400

    update styling listing page

--- public/js/app.js
+++ public/js/app.js
@@ -2,26 +2,28 @@
 // console.log($)
 // console.log(_)
 // console.log(Backbone)
-// var BACKEND_URL = "https://quik-spitter-api.herokuapp.com/api"
 
-var BACKEND_URL = ""
+
+// =======  Two different URL's to use(Travis or back-end) =======
+var BACKEND_URL = "https://quik-spitter-api.herokuapp.com/api"
+// var BACKEND_URL = ""
 
 
 var routerController = function(){
    var currentHash = window.location.hash.slice(1)
-    
+
    if(currentHash.length === 0) {
-     showHomePage()
-     var newCarForm = document.querySelector('#new-car-form')
-     newCarForm.addEventListener('submit',  saveUserCarInfo)
+     showHomePage();
+     var newCarForm = document.querySelector('#new-car-form');
+     newCarForm.addEventListener('submit',  saveUserCarInfo);
 
       return;
    }
 
-   var currentHashComponents = currentHash.split('/')
+   var currentHashComponents = currentHash.split('/');
 
-   var currentHashPrefix = currentHashComponents[0]
-   var currentHashSuffix = currentHashComponents[1]
+   var currentHashPrefix = currentHashComponents[0];
+   var currentHashSuffix = currentHashComponents[1];
 
     switch(currentHashPrefix){
 
@@ -29,23 +31,23 @@ var routerController = function(){
         $.getJSON(BACKEND_URL + "/car").then(function(serverRes){
 
           showListingPage(serverRes);
-          console.log(serverRes)
-          document.querySelector('#app-container').innerHTML
+          // console.log(serverRes)
+          document.querySelector('#app-container').innerHTML;
       })
         break;
     }
 }
 
 var saveUserCarInfo = function(evt){
-  evt.preventDefault()
+  evt.preventDefault();
 
-  document.querySelector('.car-make')
-  document.querySelector('.car-model')
-  document.querySelector('.car-year')
+  document.querySelector('.car-make');
+  document.querySelector('.car-model');
+  document.querySelector('.car-year');
 
-  var makeFormVal = evt.target.make.value
-  var modelFormVal = evt.target.model.value
-  var yearFormVal = evt.target.year.value
+  var makeFormVal = evt.target.make.value;
+  var modelFormVal = evt.target.model.value;
+  var yearFormVal = evt.target.year.value;
 
   var objForDatabase = {
     "make": makeFormVal,
@@ -53,7 +55,7 @@ var saveUserCarInfo = function(evt){
     "year": yearFormVal
   }
 
-  var jsonStr = JSON.stringify(objForDatabase)
+  var jsonStr = JSON.stringify(objForDatabase);
 
   var postReqSettings =  {
       url: BACKEND_URL + '/add-car',
@@ -68,5 +70,5 @@ var saveUserCarInfo = function(evt){
     })
 }
 
-window.addEventListener('hashchange', routerController)
-routerController()
+window.addEventListener('hashchange', routerController);
+routerController();
--- public/js/listing.js
+++ public/js/listing.js
@@ -8,31 +8,31 @@ var showListingPage =  function(carList){
 
   var bigStr = ''
   bigStr += '<div class="jumbotron main-header">'
-  bigStr += '<div class"col-sm-12><h1>Antique & Unique<br/>Cars & Coffee</h1>'
-  bigStr += '</div>'
+  bigStr +=   '<h1>Antique & Unique<br/>Cars & Coffee</h1>'
+  bigStr +=   '<h2 class="showEntrants">Entrants to the up-coming show!!</h2>'
   bigStr += '</div>'
   bigStr += '<table class="cars-attending text-center">'
   bigStr += '<thead class="text-center">'
-  bigStr += '<tr>'
-  bigStr += '<th>  Make  </th>'
-  bigStr += '<th> Model  </th>'
-  bigStr += '<th>  Year </th>'
-  bigStr += '</tr>'
+  bigStr +=   '<tr>'
+  bigStr +=     '<th class="headerNames">  Make  </th>'
+  bigStr +=     '<th class="headerNames"> Model  </th>'
+  bigStr +=     '<th class="headerNames">  Year </th>'
+  bigStr +=   '</tr>'
   bigStr += '<tbody class="car-tab">'
 
   forEach(carList,function(serverRes){
     bigStr += '<tr>'
-    bigStr +=  '<td>'+ serverRes.make + '</td>'
-    bigStr +=  '<td>' + serverRes.model + '</td>'
-    bigStr +=  '<td>'+ serverRes.year + '</td>'
+    bigStr +=  '<td class="entryRows">'+ serverRes.make + '</td>'
+    bigStr +=  '<td class="entryRows">' + serverRes.model + '</td>'
+    bigStr +=  '<td class="entryRows">'+ serverRes.year + '</td>'
     bigStr += '</tr>'
   })
 
   bigStr += '</tbody>'
   bigStr += '</table>'
 
-// })
-document.querySelector('#app-container').innerHTML = bigStr
+
+  document.querySelector('#app-container').innerHTML = bigStr
   bigStr += '</div>'
 
 
